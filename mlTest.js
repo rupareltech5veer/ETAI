a15a453314eb3a5df9fa8177c4b341eb8a3dbefb
rupareltech5veer <61360741+rupareltech5veer@users.noreply.github.com>
2021-01-17 20:26:49 -0800

    Update mlTest.js

--- mlTest.js
+++ mlTest.js
@@ -1,23 +1,26 @@
 var video = document.querySelector("#videoElement");
+var stopVideo = document.querySelector("#stop");
 
-/*if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
-      video.srcObject = stream;
+        video.srcObject = stream;
     })
     .catch(function (err0r) {
-      console.log("Something went wrong!");
+        console.log("Something went wrong!");
     });
 }
+
+stopVideo.addEventListener("click", stop, false);
 
 function stop(e) {
     var stream = video.srcObject;
     var tracks = stream.getTracks();
-  
+
     for (var i = 0; i < tracks.length; i++) {
-      var track = tracks[i];
-      track.stop();
+    var track = tracks[i];
+    track.stop();
     }
-  
+
     video.srcObject = null;
-  }*/
+}
